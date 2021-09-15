d9a76bd9bddc7687693381a6e7dd4c8d6f220569
Olayode Oluwafemi Rachael <ofemiracheal95@gmail.com>
2021-09-15 21:00:45 +0100

    defining functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,78 @@
 let inputValue = document.querySelectorAll(".calcitem");
-let calculationsection = document.querySelector('.calcsection')
+let calculationsection = document.querySelector('.calcsection');
+let answerspace = document.querySelector('.answerarea');
 
 inputValue.forEach(input => input.addEventListener('click', function(){
 
     let clickedvalue = this.textContent;
-    calculationsection.innerHTML  += clickedvalue;
+    calculationsection.textContent  += clickedvalue.padEnd(2);
   
 }));
 
+
+clear = () => {
+
+    calculationsection.textContent = " ";
+    answerspace.textContent = " ";
+
+}
+
+multiply = () => {
+
+
+}
+
+addition = () => {
+
+
+}
+
+subtraction = () => {
+
+
+}
+
+division = () => {
+
+
+}
+
+equals = () =>{
+
+
+}
+
+sincalc = () => {
+
+
+}
+
+piecalc = () => {
+
+
+}
+
+degcalc = () => {
+    
+}
+
+exponentital = () => {
+
+
+}
+
+remove = () => {
+
+
+}
+
+
+
+/*
+getting and displaying json value
+
+Object.keys(obj).forEach((key, index) => {
+    console.log(key, obj[key]);
+})
+keys, or values or entries
+*/
