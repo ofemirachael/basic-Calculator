0d9097c6041e3e32cbefcbdfdda6b8f59ae6daf5
Olayode Oluwafemi Rachael <ofemiracheal95@gmail.com>
2024-05-20 15:09:07 -0600

    done design

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let del = document.querySelector(".keydel");
 let keyReset = document.querySelector(".keyreset");
 let keySol = document.querySelector(".keysol");
 
+console.log(keybutton);
 toggleValue.addEventListener(
   "change",
   (handleThemeChange = () => {
@@ -18,14 +19,17 @@ toggleValue.addEventListener(
       calcHeading.style.color = "hsl(0, 0%, 100%)";
       theme.style.color = "hsl(0, 0%, 100%)";
       del.style.backgroundColor = "hsl(225, 21%, 49%)";
+      solutionArea.style.color = "hsl(0, 0%, 100%)";
       keyReset.style.backgroundColor = "hsl(225, 21%, 49%)";
       keySol.style.backgroundColor = "hsl(6, 63%, 50%)";
       mainbg.style.backgroundColor = "hsl(222, 26%, 31%)";
       solutionArea.style.backgroundColor = "hsl(224, 36%, 15%)";
       toggleValue.style.backgroundColor = "hsl(223, 31%, 20%)";
       keyboard.style.backgroundColor = "hsl(223, 31%, 20%)";
-      keybutton.style.backgroundColor = "hsl(0, 0%, 100%)";
-      keybutton.style.color = "hsl(221, 14%, 31%)";
+      for (j = 0; j < keybutton.length; j++) {
+        keybutton[j].style.backgroundColor = "hsl(0, 0%, 100%)";
+        keybutton[j].style.color = "hsl(221, 14%, 31%)";
+      }
     } else if (toggleValue.value === "2") {
       calcHeading.style.color = "hsl(179, 100%, 0%)";
       theme.style.color = "hsl(179, 100%, 0%)";
@@ -35,15 +39,29 @@ toggleValue.addEventListener(
       keyReset.style.backgroundColor = "hsl(185, 42%, 37%)";
       mainbg.style.backgroundColor = "hsl(0, 0%, 90%)";
       solutionArea.style.backgroundColor = "hsl(0, 0%, 100%)";
+      solutionArea.style.color = "hsl(179, 100%, 0%)";
       toggleValue.style.backgroundColor = "hsl(0, 5%, 81%)";
       keyboard.style.backgroundColor = "hsl(0, 5%, 81%)";
-      keybutton.style.backgroundColor = "hsl(0, 0%, 100%)";
-      keybutton.style.color = "hsl(221, 14%, 0%)";
+      for (i = 0; i < keybutton.length; i++) {
+        keybutton[i].style.backgroundColor = "hsl(0, 0%, 100%)";
+        keybutton[i].style.color = "hsl(221, 14%, 0%)";
+      }
     } else if (toggleValue.value === "3") {
+      solutionArea.style.color = "hsl(52, 100%, 62%)";
+      solutionArea.style.backgroundColor = "hsl(268, 71%, 12%)";
+      mainbg.style.backgroundColor = "hsl(268, 75%, 9%)";
+      keyReset.style.backgroundColor = "hsl(281, 89%, 26%)";
+      keySol.style.backgroundColor = "hsl(176, 100%, 44%)";
+      del.style.backgroundColor = "hsl(281, 89%, 26%)";
+      calcHeading.style.color = "hsl(52, 100%, 62%)";
+      theme.style.color = "hsl(52, 100%, 62%)";
+      indicator.style.color = "hsl(52, 100%, 62%)";
       toggleValue.style.backgroundColor = "hsl(268, 71%, 12%)";
       keyboard.style.backgroundColor = "hsl(268, 71%, 12%)";
-      keybutton.style.backgroundColor = "hsl(0, 0%, 100%)";
-      keybutton.style.color = "hsl(221, 14%, 31%)";
+      for (i = 0; i < keybutton.length; i++) {
+        keybutton[i].style.backgroundColor = "hsl(268, 47%, 21%)";
+        keybutton[i].style.color = "hsl(52, 100%, 62%)";
+      }
     }
   })
 );
